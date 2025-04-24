8c614bd5eeb73bf2ee8726303e723cdd2aa5e42f
Dag Efjestad <dag.efjestad@aenergi.no>
2025-04-24 14:00:57 +0200

    ENTSOE API - get plant

---
+++ dataSources/ENTSO/get_generation_per_plant.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from entsoe import EntsoePandasClient
+import os
+
+client = EntsoePandasClient(api_key=os.environ["ENTSOKEY"])
+
+start = pd.Timestamp("2025-03-01", tz="Europe/Oslo")
+end   = pd.Timestamp("2025-03-31T23:00", tz="Europe/Oslo")
+
+df = client.query_generation_per_plant("NO", start=start, end=end)
+df.to_csv("generation_pr_plant_for_march.csv", index=False)
